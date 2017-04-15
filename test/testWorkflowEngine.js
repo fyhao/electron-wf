4b88cfaf28d1819bfa4a24b7505ba04853d738b2
fyhao <fyhao1@gmail.com>
2017-04-15 20:51:01 +0800

    test http.wf

--- test/testWorkflowEngine.js
+++ test/testWorkflowEngine.js
@@ -10,8 +10,19 @@ var stepModule = ProjRequire('./step.js');
 describe('workflow_engine.js', function() {
   describe('bootstrap', function() {
 	this.timeout(15000);
-    it('should run without error', function() {
+    it('set Step module', function() {
 		workflowModule.setStepModule(stepModule);
+    });
+	it('test http.wf', function(done) {
+		var configFile = '../examples/milestone_2/issue_14/http.wf';
+		delete require.cache[require.resolve(configFile)]; // delete require cache
+		config = require(configFile); // require again
+		workflowModule.setConfig(config);
+		workflowModule.setWindow({webContents:{send:function(a,b) {}}});
+		workflowModule.executeWorkFlow(config.workFlows['TestHttp'], {}, function() {
+			console.log('done');
+			done();
+		});	
     });
   });
 });
