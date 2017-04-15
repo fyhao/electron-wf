9fdba5c282af4c6b3c83c24a842c283e5688d2ba
fyhao <fyhao1@gmail.com>
2017-04-15 21:53:34 +0800

    add copyFolder.wf

--- test/testWorkflowEngine.js
+++ test/testWorkflowEngine.js
@@ -4,22 +4,39 @@ global.ProjRequire = function(module) {
 	return require(__dirname + '/../' + module);
 }
 var workflowModule = ProjRequire('./workflow_engine.js');
+// initializing
+workflowModule.setWindow({webContents:{send:function(a,b) {}}});
 
 // BOOTSTRAPPING STEP MODULE
 var stepModule = ProjRequire('./step.js');
 describe('workflow_engine.js', function() {
+  this.timeout(15000);
   describe('bootstrap', function() {
-	this.timeout(15000);
     it('set Step module', function() {
 		workflowModule.setStepModule(stepModule);
     });
-	it('test http.wf', function(done) {
+  });
+  
+  describe('http.wf', function() {
+	it('should able to perform http operation', function(done) {
 		var configFile = '../examples/milestone_2/issue_14/http.wf';
+		delete require.cache[require.resolve(configFile)]; // delete require cache
+		var config = require(configFile); // require again
+		workflowModule.setConfig(config);
+		workflowModule.executeWorkFlow(config.workFlows['TestHttp'], {}, function() {
+			console.log('done');
+			done();
+		});	
+    });
+  });
+  
+  describe('copyFolder.wf', function() {
+	it('should able to copy folder', function(done) {
+		var configFile = '../examples/milestone_2/issue_4/copyFolder.wf';
 		delete require.cache[require.resolve(configFile)]; // delete require cache
 		config = require(configFile); // require again
 		workflowModule.setConfig(config);
-		workflowModule.setWindow({webContents:{send:function(a,b) {}}});
-		workflowModule.executeWorkFlow(config.workFlows['TestHttp'], {}, function() {
+		workflowModule.executeWorkFlow(config.workFlows['PerformWork'], {}, function() {
 			console.log('done');
 			done();
 		});	
