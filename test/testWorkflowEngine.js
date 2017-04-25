1fc40719d26983256c0ec50af8876e5249bc0eed
fyhao <fyhao1@gmail.com>
2017-04-26 00:11:55 +0800

    #27 code and test case

--- test/testWorkflowEngine.js
+++ test/testWorkflowEngine.js
@@ -601,4 +601,57 @@ describe('workflow_engine.js', function() {
 		});	
     });
   });
+  
+  describe('listFiles', function() {
+	it('should able to list files', function(done) {
+		var config = {
+			
+			workFlows : {
+				TestCase:{
+					steps : [
+						{type:'listFiles',folder:'examples/milestone_6/issue_27/first',var:'result',filter:'b'},
+						{type:'evaljs',var:'len',code:'vars["result"].length'},
+						{type:'assert',expected:2, actual:'##len##'},
+						{type:'listFiles',folder:'examples/milestone_6/issue_27/first',var:'result',filter:'1.txt',if:'endsWith'},
+						{type:'evaljs',var:'len',code:'vars["result"].length'},
+						{type:'assert',expected:2, actual:'##len##'},
+						{type:'listFiles',folder:'examples/milestone_6/issue_27/first',var:'result'},
+						{type:'evaljs',var:'len',code:'vars["result"].length'},
+						{type:'assert',expected:5, actual:'##len##'},
+					]
+				}
+			}
+		};
+		workflowModule.setConfig(config);
+		workflowModule.executeWorkFlow(config.workFlows['TestCase'], {assert:assert}, function() {
+			console.log('done');
+			done();
+		});	
+    });
+	it('should able to iterate the files in runLoop', function(done) {
+		var config = {
+			workFlows : {
+				TestCase:{
+					steps : [
+						{type:'listFiles',folder:'examples/milestone_6/issue_27/first',var:'result'},
+						{type:'evaljs',var:'len',code:'vars["result"].length'},
+						{type:'assert',expected:5, actual:'##len##'},
+						{type:'runLoop',array:'result',item:'item',wf:'eachItem'}
+					]
+				}
+				,eachItem:{
+					steps : [
+						{type:'evaljs',var:'verified',code:'vars["item"].indexOf("first") > -1'},
+						{type:'assert',expected:'1', actual:'##verified##'}
+					]
+				}
+			}
+		};
+		workflowModule.setConfig(config);
+		workflowModule.executeWorkFlow(config.workFlows['TestCase'], {assert:assert}, function() {
+			console.log('done');
+			done();
+		});	
+	});
+  });
 });
